6591d0cf1744d52fc190d5a5065eed745f9ed20d
R Davies <rd1042@login1.pri.viking.alces.network>
2022-05-19 11:21:09 +0100

    More example files to run

---
+++ test_example_scripts/feature_electromagnetic_new/run_example_2species_fieldskxkyz_nproc4_es.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=basic_job_test        # Job name
+#SBATCH --mail-type=BEGIN,END,FAIL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=bob.davies@york.ac.uk     # Where to send mail  
+#SBATCH --ntasks=4                       # Run on a single CPU
+#SBATCH --time=00:05:00                  # Time limit hrs:min:sec
+#SBATCH --cpus-per-task=1
+#SBATCH --output=array_job_%A_%a.log     # Standard output and error log
+#SBATCH --account=phys-gspt-2019         # Project account
+
+sim_name="example_2species_fieldskxkyz_es.in"
+mpirun -n 4 /users/rd1042/scratch/em_stella_new/stella $sim_name
+
