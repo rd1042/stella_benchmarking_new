cf7bc8ea99c03c5ebd7c111d32d47bc202d60765
rd1042 <rd1042@fusion213.its.york.ac.uk>
2022-09-08 22:50:10 +0100

    CBC nonlinear NISL sim

---
+++ test_cbc_nonlinear_beta0/stella_nonlinear_2species_nisl/run_stella_viking.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=basic_job_test        # Job name
+#SBATCH --mail-type=BEGIN,END,FAIL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=bob.davies@york.ac.uk     # Where to send mail  
+#SBATCH --ntasks=24                       # Run on a single CPU
+#SBATCH --time=12:00:00                  # Time limit hrs:min:sec
+#SBATCH --cpus-per-task=1
+#SBATCH --output=array_job_%A_%a.log     # Standard output and error log
+#SBATCH --account=phys-gspt-2019         # Project account
+
+sim_name="input.in"
+mpirun -n 24 /users/rd1042/scratch/stella_dev_1/stella $sim_name
+
