a473d2fa5782d6d682536f86c35d4473c5d3062b
R Davies <rd1042@login1.pri.viking.alces.network>
2022-09-10 08:25:51 +0100

    Modified/bug fixed CBC beta=0 nonlinear sims (master & NISL)

--- test_cbc_nonlinear_beta0/stella_nonlinear_2species_master/run_stella_viking.sh
+++ test_cbc_nonlinear_beta0/stella_nonlinear_2species_master/run_stella_viking.sh
@@ -4,7 +4,8 @@
 #SBATCH --mail-type=BEGIN,END,FAIL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bob.davies@york.ac.uk     # Where to send mail  
 #SBATCH --ntasks=24                       # Run on a single CPU
-#SBATCH --time=12:00:00                  # Time limit hrs:min:sec
+#SBATCH --nodes=1
+#SBATCH --time=20:00:00                  # Time limit hrs:min:sec
 #SBATCH --cpus-per-task=1
 #SBATCH --output=array_job_%A_%a.log     # Standard output and error log
 #SBATCH --account=phys-gspt-2019         # Project account
