fa3ce95bcd7840208a28a22ad16a6da0e4406a72
R Davies <rd1042@login2.pri.viking.alces.network>
2022-09-21 12:49:28 +0100

    fbpar=0 beta scans

--- w7x_em/postprocess_beta_scans.py
+++ w7x_em/postprocess_beta_scans.py
@@ -5,4 +5,5 @@ import make_beta_scans as make_scans
 
 if __name__ == "__main__":
     print("Hello world")
-    postprocess_beta_scan(make_scans.folder_name_dkh_explicit_betalr)
+    # postprocess_beta_scan(make_scans.folder_name_dkh_explicit_betalr)
+    postprocess_beta_scan(make_scans.folder_name_dkh_explicit_betalr_fbpar0)
