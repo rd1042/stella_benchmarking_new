992c00aef612b0ad78cac428874d7a1db43ae285
R Davies <rd1042@login1.pri.viking.alces.network>
2022-09-21 14:27:17 +0100

    Implicit W7-X sims

--- w7x_em/postprocess_beta_scans.py
+++ w7x_em/postprocess_beta_scans.py
@@ -6,4 +6,5 @@ import make_beta_scans as make_scans
 if __name__ == "__main__":
     print("Hello world")
     # postprocess_beta_scan(make_scans.folder_name_dkh_explicit_betalr)
-    postprocess_beta_scan(make_scans.folder_name_dkh_explicit_betalr_fbpar0)
+    # postprocess_beta_scan(make_scans.folder_name_dkh_explicit_betalr_fbpar0)
+    postprocess_beta_scan(make_scans.folder_name_dkh_implicit_betalr)
