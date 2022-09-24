6c2a69a60b16218a9c858886edc7af19886c7e75
rd1042 <rd1042@fusion213.its.york.ac.uk>
2022-09-24 18:53:50 +0100

    plotting improvements

--- test_cbc_beta0_2species/make_leapfrog_benchmark_plots_for_thesis.py
+++ test_cbc_beta0_2species/make_leapfrog_benchmark_plots_for_thesis.py
@@ -13,27 +13,9 @@ stella_master_longname = "sims/stella_master_archer2/input"
 stella_leapfrog_drifts_longname = "sims/stella_leapfrog_archer2/input"
 gs2_basecase_longname = "sims/gs2_ky05/_0.0000"
 
-def plot_phi_z_for_sim(ax1, sim_longname, sim_label, sim_type="stella", plot_format=".eps",
+def plot_phi_z_for_sim(ax1, theta, abs_phi, sim_label, sim_type="stella", plot_format=".eps",
                 linewidth=1.0, linestyleoveride=False, color=False):
     """ """
-
-    theta, real_phi, imag_phi = get_phiz_data(sim_longname, sim_type)
-    ## Check values are finite
-    if not(np.all(np.isfinite(real_phi)) and np.all(np.isfinite(imag_phi))):
-        print("Error! phi contains non-finite values")
-        print("sim_longname = ", sim_longname)
-        return
-
-    ## Combine real and imaginary parts to get abs_phi
-    # If real and imaginary parts are large, it's possible that they'll
-    # become non-finite when we square them. To avoid, perform some normalisation first
-    normalisation = np.max(abs(real_phi))
-    real_phi = real_phi/normalisation
-    imag_phi = imag_phi/normalisation
-    abs_phi = np.sqrt(real_phi*real_phi + imag_phi*imag_phi)
-
-    ## Normalise s.t. max(abs_phi) = 1
-    abs_phi = abs_phi/np.max(abs_phi)
     # Plot
     if not linestyleoveride:
         linestyle=next(linestyles2)
@@ -54,21 +36,39 @@ def make_comparison_plots_leapfrog_for_thesis(sim_longnames, sim_labels, save_na
     2) Normalised |phi|(z)
     """
 
-    mylinewidths = [15., 9, 7]
-    mycols = ["black", "cyan", "red"]
+    mylinewidths_1 = [10., 7, 5]
+    mylinewidths_2 = [15., 9, 7]
+    # mycols = ["black", "cyan", "red"]
     myaxlabelsize = 50.
-    mylegendsize = 35
+    mylegendsize = 25
     mylabelsize = 40.
     mymarkersize = 20.
-
+    my_ylabelsize = 30
     ## Plot of omega(t)
-    fig1 = plt.figure(figsize=[10, 12])
-    ax11 = fig1.add_subplot(211)
-    ax12 = fig1.add_subplot(212, sharex=ax11)
+    fig1 = plt.figure(figsize=[12, 12])
+
+    fig1_left = 0.16
+    fig1_right = 0.98
+    fig1_bottom = 0.14
+    fig1_top = 0.98
+    vspace = 0.05
+    fig1_width = fig1_right - fig1_left
+    fig1_height = (fig1_top - fig1_bottom - 2*vspace)/3
+    row2_bottom = fig1_bottom + vspace + fig1_height
+    row1_bottom = row2_bottom + vspace + fig1_height
+    ax11 = fig1.add_axes((fig1_left, row1_bottom, fig1_width, fig1_height))
+    ax12 = fig1.add_axes((fig1_left, row2_bottom, fig1_width, fig1_height))
+    ax13 = fig1.add_axes((fig1_left, fig1_bottom, fig1_width, fig1_height))
 
     ## Plot of |phi|(t)
-    fig2 = plt.figure(figsize=[17, 12])
-    ax21 = fig2.add_subplot(111)
+    fig2 = plt.figure(figsize=[12, 12])
+    fig2_left = 0.17
+    fig2_right = 0.985
+    fig2_bottom = 0.12
+    fig2_top = 0.97
+    fig2_width = fig2_right - fig2_left
+    fig2_height = fig2_top - fig2_bottom
+    ax21 = fig2.add_axes((fig2_left, fig2_bottom, fig2_width, fig2_height))
     gamma_vals = []
     freq_vals = []
 
@@ -78,6 +78,15 @@ def make_comparison_plots_leapfrog_for_thesis(sim_longnames, sim_labels, save_na
     freq_ulims = []
 
     linestyleoverides = ["-", "--", "-."]
+    sim_data_pickle = "sim_summary.pickle"
+    try:
+        myfile = open(sim_data_pickle, "rb")
+        [sim_data] = pickle.load(myfile)
+        myfile.close()
+        pickle_exists = True
+    except FileNotFoundError:
+        pickle_exists = False
+        sim_data = []
     for sim_idx, sim_longname in enumerate(sim_longnames):
         sim_label = sim_labels[sim_idx]
         # Find out the sim types - if sim_types kwarg not specified,
@@ -89,7 +98,29 @@ def make_comparison_plots_leapfrog_for_thesis(sim_longnames, sim_labels, save_na
         else:
             print("Error! len(sim_longnames), len(sim_types) = ", len(sim_longnames), len(sim_types) )
             sys.exit()
-        time, freqom_final, gammaom_final, freqom, gammaom, gamma_stable = get_omega_data(sim_longname, "stella")
+        if pickle_exists:
+            [time, freqom_final, gammaom_final, freqom, gammaom, gamma_stable, z, phi] = sim_data[sim_idx]
+        else:
+            time, freqom_final, gammaom_final, freqom, gammaom, gamma_stable = get_omega_data(sim_longname, "stella")
+            z, real_phi, imag_phi = get_phiz_data(sim_longname, sim_type)
+            ## Check values are finite
+            if not(np.all(np.isfinite(real_phi)) and np.all(np.isfinite(imag_phi))):
+                print("Error! phi contains non-finite values")
+                print("sim_longname = ", sim_longname)
+                return
+
+            ## Combine real and imaginary parts to get abs_phi
+            # If real and imaginary parts are large, it's possible that they'll
+            # become non-finite when we square them. To avoid, perform some normalisation first
+            normalisation = np.max(abs(real_phi))
+            real_phi = real_phi/normalisation
+            imag_phi = imag_phi/normalisation
+            phi = np.sqrt(real_phi*real_phi + imag_phi*imag_phi)
+
+            ## Normalise s.t. max(abs_phi) = 1
+            phi = phi/np.max(phi)
+            sim_data.append([time, freqom_final, gammaom_final, freqom, gammaom, gamma_stable, z, phi])
+
         # print("time = ", np.array(time))
         # print("freqom_final = ", np.array(freqom_final))
         # print("gammaom_final = ", np.array(gammaom_final))
@@ -105,10 +136,11 @@ def make_comparison_plots_leapfrog_for_thesis(sim_longnames, sim_labels, save_na
         freqom = np.array(freqom)
         gammaom = np.array(gammaom)
         gamma_stable = np.array(gamma_stable)
-        ax11.plot(time, freqom)
-        ax12.plot(time, gamma_stable)
-        plot_phi_z_for_sim(ax21, sim_longname, sim_label, sim_type=sim_type, linewidth=mylinewidths[sim_idx],
-                    linestyleoveride=linestyleoverides[sim_idx], color=mycols[sim_idx])
+        ax11.plot(time, freqom, linewidth=mylinewidths_1[sim_idx], ls=linestyleoverides[sim_idx])
+        ax12.plot(time, gammaom, linewidth=mylinewidths_1[sim_idx], ls=linestyleoverides[sim_idx])
+        ax13.plot(time, gamma_stable, linewidth=mylinewidths_1[sim_idx], ls=linestyleoverides[sim_idx])
+        plot_phi_z_for_sim(ax21, z, phi, sim_label, sim_type=sim_type, linewidth=mylinewidths_2[sim_idx],
+                    linestyleoveride=linestyleoverides[sim_idx])#, color=mycols[sim_idx])
 
 
         if (np.isfinite(gammaom_final) and np.isfinite(freqom_final)
@@ -128,15 +160,17 @@ def make_comparison_plots_leapfrog_for_thesis(sim_longnames, sim_labels, save_na
     freq_ulim = np.max(np.array(freq_ulims))*1.1
     # ax11.set_ylim(freq_llim, freq_ulim)
     # ax12.set_ylim(gamma_llim, gamma_ulim)
-    ax11.set_ylim(0.15, 0.3)
+    ax11.set_ylim(0.16, 0.29)
     ax12.set_ylim(0.1, 0.25)
-    ax21.set_ylim(-0.05, 1.19)
+    ax13.set_ylim(0.165, 0.195)
+    ax21.set_ylim(-0.05, 1.05)
     ax21.set_xlim(-6, 6)
-    ax12.set_xlabel(r"$t$", fontsize=myaxlabelsize)
-    ax11.set_ylabel(r"$\omega$", fontsize=myaxlabelsize)
-    ax12.set_ylabel(r"$\gamma$", fontsize=myaxlabelsize)
+    ax13.set_xlabel(r"$\tilde{t}$", fontsize=myaxlabelsize)
+    ax11.set_ylabel(r"$\tilde{\omega}_{stella}$", fontsize=myaxlabelsize)
+    ax12.set_ylabel(r"$\tilde{\gamma}_{stella}$", fontsize=myaxlabelsize)
+    ax13.set_ylabel(r"$\tilde{\gamma}_{2}$", fontsize=myaxlabelsize)
     ax21.set_xlabel(r"$z/\pi$", fontsize=myaxlabelsize)
-    ax21.set_ylabel(r"$\vert \phi \vert$", fontsize=myaxlabelsize)
+    ax21.set_ylabel(r"$\vert \tilde{\phi}_k \vert$", fontsize=myaxlabelsize)
     axes = [ax21]
 
     ax21.legend(loc="upper left", fontsize=mylegendsize, fancybox=True, framealpha=0.5)
@@ -148,13 +182,37 @@ def make_comparison_plots_leapfrog_for_thesis(sim_longnames, sim_labels, save_na
         ax.tick_params(axis='both', which='major', labelsize=mylabelsize, direction="in")
         ax.tick_params(axis='both', which='minor', direction="in")
 
-    for fig in [fig2]:
-        fig.tight_layout()
+    axes = [ax11, ax12, ax13]
+    for ax in axes:
+        ax.set_xlim(0,125)
+        ax.grid(True)
+        ax.tick_params(axis='both', which='major', labelsize=mylabelsize, direction="in")
+        ax.tick_params(axis='both', which='minor', direction="in")
+        ax.set_xticks([0, 40, 80, 120])
+
+    ax13.set_xticklabels([r"$0$", r"$40$", r"$80$", r"$120$"], fontsize=mylabelsize )
+    ax21.set_xticks([-6, -4, -2, 0, 2, 4, 6] )
+    ax21.set_xticklabels([r"$-6$", r"$-4$", r"$-2$", r"$0$", r"$2$", r"$4$", r"$6$"], fontsize=mylabelsize )
+
+    ax11.set_yticks([0.2, 0.25])
+    ax11.set_yticklabels([r"$0.2$", r"$0.25$"], fontsize=my_ylabelsize )
+    ax12.set_yticks([0.15, 0.2])
+    ax12.set_yticklabels([r"$0.15$", r"$0.2$"], fontsize=my_ylabelsize )
+    ax13.set_yticks([0.17, 0.18, 0.19])
+    ax13.set_yticklabels([r"$0.17$", r"$0.18$", r"$0.19$"], fontsize=my_ylabelsize )
+    for ax in [ax11, ax12]:
+        ax.set_xticklabels([])
+
     #plt.show()
     fig1.savefig(save_name + "_omega" + ".eps")
     fig2.savefig(save_name + "_phi" + ".eps")
     plt.close(fig1)
     plt.close(fig2)
+
+    if not pickle_exists:
+        myfile = open(sim_data_pickle, "wb")
+        pickle.dump([sim_data],myfile)
+        myfile.close()
     return
 
 
@@ -162,20 +220,23 @@ def compare_stella_leapfrog_gs2_for_thesis():
     """ """
 
     make_comparison_plots_leapfrog_for_thesis([
+            gs2_basecase_longname,
             stella_master_longname,
             stella_leapfrog_drifts_longname,
-            gs2_basecase_longname,
                     ],
                     [
-                    r"stella;$\Omega$=$0.206$+$0.184$i",
-                    r"stella (Multipstep);$\Omega$=$0.12$+$0.14$i",
-                    r"GS2;$\Omega$=$0.202$+$0.187$i",
+                    r"GS2",#;$\Omega$=$0.202$+$0.187$i",
+                    r"stella",#;$\Omega$=$0.206$+$0.184$i",
+                    r"stella (Multipstep)"#";$\Omega$=$0.201$+$0.138$i",
+                    # r"GS2;$\Omega$=$0.202$+$0.187$i",
+                    # r"stella;$\Omega$=$0.206$+$0.184$i",
+                    # r"stella (Multipstep);$\Omega$=$0.201$+$0.138$i",
                     ],
-                    "./poster_leapfrog",
+                    "./linear_leapfrog_benchmark",
                     sim_types=[
-                    "stella",
-                    "stella",
                     "gs2",
+                    "stella",
+                    "stella",
                     ],
                     )
     return
