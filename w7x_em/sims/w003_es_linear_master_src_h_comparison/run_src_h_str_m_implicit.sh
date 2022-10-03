29fa406efb7c1a59c99cc616dfc96983d8397174
R Davies <rd1042@login1.pri.viking.alces.network>
2022-10-03 07:58:39 +0100

    w7x es/em stella benchmarking

--- w7x_em/sims/w003_es_linear_master_src_h_comparison/run_src_h_str_m_implicit.sh
+++ w7x_em/sims/w003_es_linear_master_src_h_comparison/run_src_h_str_m_implicit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=basic_job_test        # Job name
 #SBATCH --mail-type=BEGIN,END,FAIL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bob.davies@york.ac.uk     # Where to send mail  
-#SBATCH --ntasks=1                       # Run on a single CPU
+#SBATCH --ntasks=4                       # Run on a single CPU
 #SBATCH --time=6:00:00                  # Time limit hrs:min:sec
 #SBATCH --cpus-per-task=1
 #SBATCH --output=array_job_%A_%a.log     # Standard output and error log
