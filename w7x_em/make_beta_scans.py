31b9b0f61ef58f5f7545eb660c4d4a0758043f2a
R Davies <rd1042@login2.pri.viking.alces.network>
2022-10-03 21:00:43 +0100

    More fiddling with EM w7-x sims

--- w7x_em/make_beta_scans.py
+++ w7x_em/make_beta_scans.py
@@ -16,13 +16,18 @@ folder_name_dkh_implicit_betalr = "sims/wout_DKH_0_norm_betalr_scan_implicit"
 folder_name_dkh_explicit_betalr_fbpar0 = "sims/wout_DKH_0_norm_betalr_scan_fbpar0_explicit"
 folder_name_w003_explicit = "sims/w003_beta_scan_explicit"
 folder_name_w003_implicit_ky1 = "sims/w003_beta_scan_implicit_ky1"
-folder_name_w003_implicit_ky1_upw0 = "sims/w003_beta_scan_implicit_ky1_upw0"
 folder_name_w003_implicit_ky2 = "sims/w003_beta_scan_implicit_ky2"
 folder_name_w003_implicit_ky15 = "sims/w003_beta_scan_implicit_ky1.5"
 folder_name_w003_explicit_fbpar0 = "sims/w003_beta_scan_explicit_fbpar0"
 folder_name_w003_implicit_ky1_fbpar0 = "sims/w003_beta_scan_implicit_ky1_fbpar0"
 folder_name_w003_implicit_ky2_fbpar0 = "sims/w003_beta_scan_implicit_ky2_fbpar0"
 folder_name_w003_implicit_ky15_fbpar0 = "sims/w003_beta_scan_implicit_ky1.5_fbpar0"
+
+folder_name_w003_implicit_ky1_upw0 = "sims/w003_beta_scan_implicit_ky1_upw0"
+folder_name_w003_implicit_ky1_fbpar0_upw0 = "sims/w003_beta_scan_implicit_ky1_fbpar0_upw0"
+folder_name_w003_implicit_ky15_upw0 = "sims/w003_beta_scan_implicit_ky1.5_upw0"
+folder_name_w003_implicit_ky2_upw0 = "sims/w003_beta_scan_implicit_ky2_upw0"
+# folder_name_w003_implicit_ky1_upw0 = "sims/w003_beta_scan_implicit_ky1_upw0"
 
 if __name__ == "__main__":
     print("Hello world")
@@ -39,4 +44,8 @@ if __name__ == "__main__":
     # make_beta_scan(folder_name_w003_implicit_ky15_fbpar0, beta_vals)
     # make_beta_scan(folder_name_w003_implicit_ky2, beta_vals)
     # make_beta_scan(folder_name_w003_implicit_ky2_fbpar0, beta_vals)
-    make_beta_scan(folder_name_w003_implicit_ky1_upw0, beta_vals)
+
+    # make_beta_scan(folder_name_w003_implicit_ky1_upw0, beta_vals)
+    make_beta_scan(folder_name_w003_implicit_ky1_fbpar0_upw0, beta_vals)
+    make_beta_scan(folder_name_w003_implicit_ky15_upw0, beta_vals)
+    make_beta_scan(folder_name_w003_implicit_ky2_upw0, beta_vals)
