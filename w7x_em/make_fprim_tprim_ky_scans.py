71dc917e5354b0d4157ede08297b5a555dee52a4
rd1042 <rd1042@fusion213.its.york.ac.uk>
2022-10-04 12:53:04 +0100

    w003 stability valley at finite beta

--- w7x_em/make_fprim_tprim_ky_scans.py
+++ w7x_em/make_fprim_tprim_ky_scans.py
@@ -17,9 +17,11 @@ folder_name_expl_higher_ky_em = "sims/kjm3_em_linear_fprim_tprim_explicit_scan_h
 folder_name_expl_higher_ky_es = "sims/kjm3_es_linear_fprim_tprim_explicit_scan_higher_ky"
 folder_name_impl_higher_ky_em = "sims/kjm3_em_linear_fprim_tprim_implicit_scan_higher_ky"
 folder_name_impl_higher_ky_es = "sims/kjm3_es_linear_fprim_tprim_implicit_scan_higher_ky"
-folder_name_impl_em_good_resolution = "sims/kjm3_em_linear_fprim_tprim_implicit_scan_good_resolution"
-folder_name_impl_em_good_resolution_beta001 = "sims/kjm3_em_linear_fprim_tprim_implicit_scan_good_resolution_beta0.01"
-folder_name_impl_em_good_resolution_beta003 = "sims/kjm3_em_linear_fprim_tprim_implicit_scan_good_resolution_beta0.03"
+kjm3_impl_em_good_resolution = "sims/kjm3_em_linear_fprim_tprim_implicit_scan_good_resolution"
+kjm3_impl_em_good_resolution_beta001 = "sims/kjm3_em_linear_fprim_tprim_implicit_scan_good_resolution_beta0.01"
+kjm3_impl_em_good_resolution_beta003 = "sims/kjm3_em_linear_fprim_tprim_implicit_scan_good_resolution_beta0.03"
+w003_impl_em_good_resolution_beta001 = "sims/w003_em_linear_fprim_tprim_implicit_scan_good_resolution_beta0.01"
+w003_impl_em_good_resolution_beta003 = "sims/w003_em_linear_fprim_tprim_implicit_scan_good_resolution_beta0.03"
 
 if __name__ == "__main__":
     print("Hello world")
@@ -35,6 +37,6 @@ if __name__ == "__main__":
     # make_fprim_tprim_ky_scan(folder_name_expl_higher_ky_es, fprim_vals, tprim_vals, ky_vals)
     # make_fprim_tprim_ky_scan(folder_name_impl_higher_ky_em, fprim_vals, tprim_vals, ky_vals)
     # make_fprim_tprim_ky_scan(folder_name_impl_higher_ky_es, fprim_vals, tprim_vals, ky_vals)
-    # make_fprim_tprim_ky_scan(folder_name_impl_em_good_resolution, fprim_vals, tprim_vals, ky_vals)
-    make_fprim_tprim_ky_scan(folder_name_impl_em_good_resolution_beta001, fprim_vals, tprim_vals, ky_vals)
-    make_fprim_tprim_ky_scan(folder_name_impl_em_good_resolution_beta003, fprim_vals, tprim_vals, ky_vals)
+    # make_fprim_tprim_ky_scan(kjm3_impl_em_good_resolution, fprim_vals, tprim_vals, ky_vals)
+    make_fprim_tprim_ky_scan(kjm3_impl_em_good_resolution_beta001, fprim_vals, tprim_vals, ky_vals)
+    make_fprim_tprim_ky_scan(kjm3_impl_em_good_resolution_beta003, fprim_vals, tprim_vals, ky_vals)
